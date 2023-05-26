754ba75e95aaa925d80cb231a341b46ddb2ab07b
Kartik Vadhawana <kartikvadhwana7@gmail.com>
2023-05-26 13:35:01 +0530

    mongoose connection (mongodb and nodejs)

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("app is working");
-});
+const mongoose = require('mongoose');
+const connectDB = async()=>{
+    mongoose.connect('mongodb://localhost:27017/e-comm');
+    const productSchema = new mongoose.Schema({});
+    const product = mongoose.model('product',productSchema,'products');
+    const data = await product.find();
+    console.log(data);
+}
+connectDB();
+
+// app.get('/',(req,res)=>{
+//     res.send("app is working");
+// });
 
 app.listen(5000);
