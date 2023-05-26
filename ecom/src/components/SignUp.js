9e1d319f92495d1591741207088355e8301b9933
Kartik Vadhawana <kartikvadhwana7@gmail.com>
2023-05-26 17:26:45 +0530

    fetch api and store data in DB

--- ecom/src/components/SignUp.js
+++ ecom/src/components/SignUp.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const SignUp = ()=>{
     const [name,setName] = useState("");
     const [email,setEmail] = useState("");
     const [pswd,setPswd] = useState("");
-    const collectData = ()=>{
+    const navigate = useNavigate();
+    const collectData = async ()=>{
         console.log(name,email,pswd);
+        let result = await fetch('http://localhost:5000/register',{ //we can axios also ..fetch is a function/api for 
+            method:'POST',
+            body : JSON.stringify({name,email,pswd}),
+            headers:{
+                'Content-Type':'application/json'
+            },
+            //Sets the 'Content-Type' header to 'application/json', indicating that the request body is in JSON format.
+        });
+        result = await result.json() //The code uses the json method on the result object to parse the response body as JSON. The json method returns a new Promise that resolves to the JavaScript object corresponding to the JSON data in the response body. By using await keyword, the code waits for the json method to complete and assigns the parsed result to the result variable.
+        console.log(result);
+        if(result)
+        {
+            navigate('/products');
+        }
         setTimeout(()=>{ //to delay the state updates slightly
             setName("");
             setEmail("");
